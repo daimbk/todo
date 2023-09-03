86a8f41ac0044eb43c0be5984a3bb3d3a6f68cc0
Daim Bin Khalid <daimbkhalid@gmail.com>
2023-09-03 15:45:32 +0500

    check pre-existing path

--- scripts/createAlias.bat
+++ scripts/createAlias.bat
@@ -1,7 +1,12 @@
 @echo off
-mkdir "c:\aliases"
-setx PATH "c:\aliases;%PATH%"
 
+:: check if c:\aliases is already in the PATH
+echo %PATH% | findstr /i "\<c:\\aliases\>" > nul
+if errorlevel 1 (
+    setx PATH "c:\aliases;%PATH%"
+)
+
+mkdir "c:\aliases"
 set CURRENT_DIR=%CD%
 echo @echo off > "c:\aliases\todo.bat"
 echo pushd "%CURRENT_DIR%" >> "c:\aliases\todo.bat"
